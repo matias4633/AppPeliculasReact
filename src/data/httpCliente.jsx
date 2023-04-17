1e51ed40953d66cd2b7700c2644c1635f0bb026d
Matias Basilotta <matiasbasilotta@gmail.com>
2023-04-16 22:52:30 -0300

    Deploy

--- src/data/httpCliente.jsx
+++ src/data/httpCliente.jsx
@@ -1,21 +1,23 @@
-const API = "https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/";
+const API = "https://api.themoviedb.org/3/";
 const API_KEY = "?api_key=f46ff8c86688e5aadcab58619a8e41e4";
 const pagina = '&language=en-US&page=1';
+const CORS = "https://cors-anywhere.herokuapp.com/";
 export function get(path) {
     // console.log(API + path + API_KEY);
-  return fetch(API + path + API_KEY, {
+  return fetch(CORS + API + path + API_KEY, {
     mode: "cors",
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Authorization": "Bearer f46ff8c86688e5aadcab58619a8e41e4"
     }
   }).then((result) => result.json());
 }
 export function getSimilarMovies(movieId){
   const path = `movie/${movieId}/similar`;
   console.log(API + path + API_KEY);
-  return fetch(API + path + API_KEY, {
+  return fetch(CORS + API + path + API_KEY, {
     mode: "cors",
     method: "GET",
     headers: {
